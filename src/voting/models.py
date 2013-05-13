f96a763c93b934a411d3f2e6094a603d16a8bacd
Andrii Dobrovolskyi <rain.ua@gmail.com>
2013-05-13 20:15:36 +0300

    Updated version (checking by positive/negative) of is_upvote() and is_downvote() have been added into model

--- src/voting/models.py
+++ src/voting/models.py
@@ -47,3 +47,10 @@ class Vote(models.Model):
             voting.signals.change_vote.send(sender=self.object, vote=self)
         super(Vote, self).save(*args, **kwargs)
 
+    def is_upvote(self):
+        return self.vote > 0
+
+    def is_downvote(self):
+        return self.vote < 0
+
+
